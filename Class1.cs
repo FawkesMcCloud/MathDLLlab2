d989b54cd3c03324f1dfcd42f69f1b9c0c372b5d
FawkesMcCloud <mypp32@gmail.com>
2021-03-01 19:25:36 +0700

    Added UnitTests

--- Class1.cs
+++ Class1.cs
@@ -17,155 +17,82 @@ static public class MathLab2
 {
     private static readonly double PI = 3.14159265359;
 
-    static public double add(double[] args)
+    //Addition 1
+    static public double Add(double a, double b)
     {
-        double sum = 0;
-        foreach(double val in args)
-            sum += val;
-
-        return sum;
-    }
-    static public float add(float[] args)
-    {
-        float sum = 0;
-        foreach (float val in args)
-            sum += val;
-
-        return sum;
-    }
-    static public int add(int[] args)
-    {
-        int sum = 0;
-        foreach (int val in args)
-            sum += val;
-
-        return sum;
+        return a + b;
     }
 
 
-    static public double sub(double[] args)
+    //Substraction 1
+    static public double Sub(double a, double b)
     {
-        double sum = 0;
-        foreach (double val in args)
-            sum -= val;
-
-        return sum;
-    }
-    static public float sub(float[] args)
-    {
-        float sum = 0;
-        foreach (float val in args)
-            sum -= val;
-
-        return sum;
-    }
-    static public int sub(int[] args)
-    {
-        int sum = 0;
-        foreach (int val in args)
-            sum -= val;
-
-        return sum;
+        return a - b;
     }
 
 
-    static public double mul(double[] args)
+    //Multiplication 1
+    static public double Mul(double a, double b)
     {
-        double prod = 0;
-        foreach (double val in args)
-            prod *= val;
-
-        return prod;
-    }
-    static public float mul(float[] args)
-    {
-        float prod = 0;
-        foreach (float val in args)
-            prod *= val;
-
-        return prod;
-    }
-    static public int mul(int[] args)
-    {
-        int prod = 0;
-        foreach (int val in args)
-            prod *= val;
-
-        return prod;
+        return a * b;
     }
 
 
-    static public double div(double[] args)
+    //Division 1
+    static public double Div(double a, double b)
     {
-        double prod = args[0];
-        for(int i = 1; i < args.Length-1; i++)
-        {
-            if (args[i] == 0.0)
-                return double.NaN;
+        if (b == 0.0)
+            return double.NaN;
 
-            prod /= args[i];
-        }
-
-        return prod;
-    }
-    static public double div(float[] args)
-    {
-        double prod = args[0];
-        for (int i = 1; i < args.Length - 1; i++)
-        {
-            if (args[i] == 0.0)
-                return double.NaN;
-
-            prod /= args[i];
-        }
-
-        return prod;
-    }
-    static public double div(int[] args)
-    {
-        double prod = args[0];
-        for (int i = 1; i < args.Length - 1; i++)
-        {
-            if (args[i] == 0.0)
-                return double.NaN;
-
-            prod /= args[i];
-        }
-
-        return prod;
+        else return a / b;
     }
 
 
-    static public double pow(int num, int exponent)
+    //Math fucnt 2
+    static public double Pow(double num, int exponent)
     {
+        if (exponent == 0 && num == 0)
+            return double.NaN;
         if (exponent == 0)
-            return 0;
+            return 1;
 
         double res = num;
 
-        for (int i = 1; i < exponent; i++)
+        for (int i = 1; i < MathLab2.Abs(exponent); i++)
             res *= num;
 
         if (exponent < 0)
             return 1.0 / res;
         return res;
     }
-    
-    static public double getPerimiterRect(double a, double b)
+    static public double Abs(double a)
+    {
+        if (a < 0)
+            return -a;
+        return a;
+    }
+
+
+    //Rectangle 2
+    static public double PerimiterRect(double a, double b)
     {
         return a + a + b + b;
     }
-    static public double getPerimiterTriag(double a, double b, double c)
+    static public double AreaRect(double a, double b)
+    {
+        return a * b;
+    }
+
+
+    //Triagle 2
+    static public double TriagPerimiter(double a, double b, double c)
     {
         return a + b + c;
     }
-    static public double getCircumference(double r)
+
+    static public double TriagArea(double _base, double height)
     {
-        return 2 * PI * r;
-    }
-    static public double TriagArea(double b, double h)
-    {
-        return h * b / 2.0;
+        return height * _base / 2.0;
     }
     static public double TriagArea(double a, double b, double c)
     {
@@ -174,12 +101,71 @@ static public class MathLab2
 
         return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
     }
-    static public double AreaRect(double a, double b)
-    {
-        return a * b;
-    }
     static public double TriagArea(double a, double b, Angle angle)
     {
         return a * b * Math.Sin(angle.angle) / 2;
     }
+
+
+    //Circle\Ellipse 5
+    static public double Circumference(double r)
+    {
+        return 2 * MathLab2.PI * r;
+    }
+
+    static public double CircleArea(double r)
+    {
+        return r * r * MathLab2.PI;
+    }
+    static public double EllipseArea(double a, double b)
+    {
+        return a * b * MathLab2.PI;
+    }
+    static public double Eccentricity(double a, double b)
+    {
+        if(a > b)
+            return LinearEccentricity(a, b) / a;
+
+        return LinearEccentricity(b, a) / b;
+
+    }
+    static public double LinearEccentricity(double a, double b)
+    {
+        return Math.Sqrt(a * a - b * b);
+    }
+
+
+    //3D Shapes 6
+    static public double EllipseVol(double a, double b, double c)
+    {
+        return (a * b * c) * (4 * MathLab2.PI / 4 * 0.25);
+    }
+    static public double SphereVol(double r)
+    {
+        return r * r * r * (4 * MathLab2.PI / 4 * 0.25);
+    }
+
+    static public double EllipseMassUniform(double p, double volume)
+    {
+        return p * volume;
+    }
+    static public double EllipseMassUniform(double p, double a, double b, double c)
+    {
+        return p * MathLab2.EllipseVol(a, b, c);
+    }
+
+    static public double SphereSurfArea(double r)
+    {
+        return 4 * MathLab2.PI * r * r;
+    }
+
+    static public double CubeSurfArea(double a)
+    {
+        return 6 * MathLab2.AreaRect(a, a);
+    }
+    static public double CuboidSurfArea(double w, double h, double l)
+    {
+        return 2 * (l * w + w * h + h * l);
+    }
+
 }
